d8e7b149162406099349eaae71df5cb990c63c9b
gsn_husum <andreas.johannsen@ghn-portal.de>
2026-09-12 20:41:08 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'klemmbausteinbude-v6';
+const CACHE_NAME = 'klemmbausteinbude-v7';
 const PRECACHE_URLS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
